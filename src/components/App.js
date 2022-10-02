8346626dd44d8fef96cfdda04d6701b631e37161
Ryan Short <shorttryan@gmail.com>
2022-10-01 20:44:04 -0400

    adds restaurant logo function

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+import LogoContainer from './LogoImage';
+
+
+function App() {
+  return (
+    <div className="page-container">
+      <div className="app-container">
+      <LogoContainer />
+      <>Menu Items Will Go Here</>
+      <>Food Type Items Will Go Here</>
+      <>Order Cart Here</>
+           
+
+
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LogoImage.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Container from 'react-bootstrap/Container';
+
+ 
+
+function LogoContainer() {
+    
+    
+    
+  
+    
+    return (
+        
+            <Container className="col-12">
+                 <img src={"./Images/vicspizza.png"} className="img-fluid"/>
+            </Container>
+
+        
+    )
+
+
+
+};
+
+export default LogoContainer;
